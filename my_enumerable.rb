06e760e56a30d032fa74e87288823cfd9cff6a84
habasm <habasm365@gmail.com>
2023-04-06 13:55:09 +0300

    linters errors fixation of files

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,19 +1,17 @@
 module MyEnumerable
-    def all?(&block)
-      result = true
-      each { |e| result = false unless block.call(e) }
-      result
-    end
-  
-    def any?(&block)
-      result = false
-      each { |e| result = true if block.call(e) }
-      result
-    end
-  
-    def filter?(&block)
-      result = []
-      each { |e| result << e if block.call(e) }
-      result
-    end
+  def all?(&block)
+    each { |item| return false unless block.call(item) }
+    true
   end
+
+  def any?(&block)
+    each { |item| return true if block.call(item) }
+    false
+  end
+
+  def filter(&block)
+    result = []
+    each { |item| result << item if block.call(item) }
+    result
+  end
+end
--- my_list.rb
+++ my_list.rb
@@ -11,12 +11,3 @@ class MyList
     @list.each(&block)
   end
 end
-
-
-list = MyList.new(1, 2, 3, 4)
-print(list.all? {|e| e < 5})
-print(list.all? {|e| e > 5})
-print(list.any? {|e| e == 2})
-print(list.any? {|e| e == 5})
-
-print(list.filter?{|e| e.even?})
